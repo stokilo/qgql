895237c74fd7f458b9601d423d0baba29fddd739
Slawomir Stec <slawomirstec@gazeta.pl>
2023-02-26 14:57:20 +0400

    QGQL.

--- src/main/resources/db/migration/V1.0.0__Quarkus.sql
+++ src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -1,7 +1,17 @@
-CREATE TABLE quarkus
+CREATE TABLE TODO_LIST
 (
-    id   INT,
+    id   INT PRIMARY KEY,
     name VARCHAR(20)
 );
-INSERT INTO quarkus(id, name)
-VALUES (1, 'QUARKED');
+CREATE TABLE TODO_LIST_ITEM
+(
+    id   INT PRIMARY KEY,
+    listid int references TODO_LIST(id),
+    text VARCHAR(255)
+);
+
+INSERT INTO TODO_LIST(id, name)
+VALUES (1, 'MyTodoList');
+
+INSERT INTO TODO_LIST_ITEM(id, listid, name)
+VALUES (1, 1,  'Item1');
