f12eea8bc8cf5823715d2a1eee5d53fcfddbdde7
Slawomir Stec <slawomirstec@gazeta.pl>
2023-02-26 17:03:24 +0400

    QGQL.

--- web/src/pages/todo1/todo-form-control.tsx
+++ web/src/pages/todo1/todo-form-control.tsx
@@ -1,39 +1,39 @@
-// import * as React from 'react'
-// import { useForm } from 'react-hook-form'
-// import { CreateTodoListRequest, CreateTodoListResponse } from '../../../../generated/api'
-// import { zodResolver } from '@hookform/resolvers/zod'
-// import { createTodoListRequestSchema } from '@pg/services/common/schemas/todo-schema'
-// import TodoFormView from './todo-form-view'
-//
-// type Props = {
-//   onSubmit: (data: CreateTodoListRequest) => Promise<CreateTodoListResponse | undefined>
-// }
-//
-// export default function TodoFormControl({ onSubmit }: Props) {
-//   const form = useForm<CreateTodoListRequest>({
-//     mode: 'onSubmit',
-//     defaultValues: { listName: '' },
-//     resolver: zodResolver(createTodoListRequestSchema),
-//   })
-//
-//   const handleSubmit = async (data: CreateTodoListRequest) => {
-//     console.log('submit')
-//     try {
-//       const result = await onSubmit(data)
-//       if (result) {
-//         if (result.hasError) {
-//           result.fieldErrors.forEach((e) => {
-//             // @ts-ignore
-//             form.setError(e!.path, {
-//               message: e!.message,
-//             })
-//           })
-//         }
-//       }
-//     } catch (e) {
-//       console.dir(e)
-//     }
-//   }
-//
-//   return <TodoFormView form={form} onSubmit={handleSubmit} />
-// }
+import * as React from 'react'
+import { useForm } from 'react-hook-form'
+import TodoFormView from './todo-form-view'
+import {TodoListItemCreationResult, TodoListItemInput} from "../../../generated/api";
+
+type Props = {
+  onSubmit: (data: TodoListItemInput) => Promise<TodoListItemCreationResult | undefined>
+}
+
+export default function TodoFormControl({ onSubmit }: Props) {
+  const form = useForm<TodoListItemInput>({
+    mode: 'onSubmit',
+    defaultValues: {
+        headline: 'default-headline'
+    }
+  })
+
+  const handleSubmit = async (data: TodoListItemInput) => {
+    console.log('submit')
+    try {
+      const result = await onSubmit(data)
+      if (result) {
+        if (!result.success) {
+            // form.setError(result.error)
+          // result.forEach((e) => {
+          //   // @ts-ignore
+          //   form.setError(e!.path, {
+          //     message: e!.message,
+          //   })
+          // })
+        }
+      }
+    } catch (e) {
+      console.dir(e)
+    }
+  }
+
+  return <TodoFormView form={form} onSubmit={handleSubmit} />
+}
