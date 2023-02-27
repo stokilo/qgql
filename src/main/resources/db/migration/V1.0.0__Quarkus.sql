271cb9e37f0deab65bf2ad96713fb4660e97fe39
Slawomir Stec <slawomirstec@gazeta.pl>
2023-02-27 19:52:35 +0400

    QGQL.

--- src/main/resources/db/migration/V1.0.0__Quarkus.sql
+++ src/main/resources/db/migration/V1.0.0__Quarkus.sql
@@ -1,23 +1,16 @@
+create sequence hibernate_sequence start 1 increment 1;
+
 create table todo_list (
-   id serial primary key,
+   id int8 primary key,
    name varchar not null
 );
 
 create table todo_item (
-    id serial primary key,
+    id int8 primary key,
     headline text not null,
     body text,
-    list_id int4 references todo_list(id) ON UPDATE CASCADE ON DELETE RESTRICT
+    list_id int8 references todo_list(id) ON UPDATE CASCADE ON DELETE RESTRICT
 );
 
-alter sequence todo_list_id_seq restart with 1;
-
-insert into todo_list(id,name) values (nextval('todo_list_id_seq'), 'list 1 user 1');
-insert into todo_list(id,name) values (nextval('todo_list_id_seq'), 'list 2 user 1');
-insert into todo_list(id,name) values (nextval('todo_list_id_seq'), 'list 1 user 2');
-insert into todo_list(id,name) values (nextval('todo_list_id_seq'), 'list 2 user 2');
-
-insert into todo_item(headline, body, list_id) values ('h1', 'b1', 1);
-insert into todo_item(headline, body, list_id) values ('h2', 'b2', 2);
-insert into todo_item(headline, body, list_id) values ('h2', 'b2', 3);
-insert into todo_item(headline, body, list_id) values ('h2', 'b2', 4);
+insert into todo_list(id,name) values (1, 'list 1 user 1');
+insert into todo_item(id, headline, body, list_id) values (1, 'h1', 'b1', 1);
