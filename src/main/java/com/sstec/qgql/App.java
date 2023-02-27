5eb4f516f01c6dfa551232c4dc943881ade9332f
Slawomir Stec <slawomirstec@gazeta.pl>
2023-02-27 21:05:05 +0400

    QGQL.

--- src/main/java/com/sstec/qgql/App.java
+++ src/main/java/com/sstec/qgql/App.java
@@ -28,15 +28,15 @@ public class App {
                         @Override
                         public void run() {
                             try {
-//                                log.info("Generate schmea under web/generated dir");
-//                                ProcResult schema = new ProcBuilder("curl", "" +
-//                                        "http://localhost:8080/q/openapi").run();
-//                                Files.write(Paths.get("web/generated/schema.yaml"), schema.getOutputBytes());
-//
-//                                log.info("Start conversion from gql to ts");
-//                                ProcResult output = new ProcBuilder("yarn", "openapi-generate")
-//                                        .withWorkingDirectory(new File("web")).run();
-//                                log.info(output.getOutputString());
+                                log.info("Generate schmea under web/generated dir");
+                                ProcResult schema = new ProcBuilder("curl", "" +
+                                        "http://localhost:8080/q/openapi").run();
+                                Files.write(Paths.get("web/generated/schema.yaml"), schema.getOutputBytes());
+
+                                log.info("Start conversion from gql to ts");
+                                ProcResult output = new ProcBuilder("yarn", "openapi-generate")
+                                        .withWorkingDirectory(new File("web")).run();
+                                log.info(output.getOutputString());
                             } catch (Exception e) {
                                 log.error(e.getMessage(), e);
                             }
