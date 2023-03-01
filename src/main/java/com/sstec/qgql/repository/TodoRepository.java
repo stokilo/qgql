f8bcaa4315eb11d4678c85a7260239e04b16745d
Slawomir Stec <slawomirstec@gazeta.pl>
2023-03-01 08:46:09 +0400

    QGQL.

--- src/main/java/com/sstec/qgql/repository/TodoRepository.java
+++ src/main/java/com/sstec/qgql/repository/TodoRepository.java
@@ -13,21 +13,6 @@ public class TodoRepository {
 
     @Transactional
     public List<TodoList> newAndReturnAll() {
-        TodoItem item1 = new TodoItem();
-        item1.body = "item1-body";
-        item1.headline = "item1-headline";
-
-        TodoItem item2 = new TodoItem();
-        item2.body = "item2-body";
-        item2.headline = "item2-headline";
-
-        TodoList l = new TodoList();
-        l.name = UUID.randomUUID().toString();
-        l.addItem(item1);
-        l.addItem(item2);
-        l.persist();
-
-        List<TodoList> list = TodoList.listAll();
-        return list;
+        return TodoList.listAll();
     }
 }
