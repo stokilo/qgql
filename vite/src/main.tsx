eb15105c5afbd92540ba4de4d113b2b664d1d274
Slawomir Stec <st@consol.ae>
2023-04-18 09:39:03 +0400

    Add vite.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import TodoPage from "./todo/todo";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import axios from "axios";
+
+const queryClient = new QueryClient();
+axios.defaults.baseURL = 'http://localhost:8080';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <TodoPage />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <TodoPage/>
+        </QueryClientProvider>
+    </React.StrictMode>,
 )
