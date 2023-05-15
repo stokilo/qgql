059154ceb0bb30edb5db14bedbcd55588fac851c
Slawomir Stec <slawomirstec@gazeta.pl>
2023-05-15 19:41:27 +0400

    Add dashboard.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import TodoPage from "./todo/todo";
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import axios from "axios";
+import Dashboard from "./dashboard/Dashboard";
 
 const queryClient = new QueryClient();
 axios.defaults.baseURL = 'http://localhost:8080';
@@ -11,7 +12,7 @@ axios.defaults.baseURL = 'http://localhost:8080';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <TodoPage/>
+            <Dashboard/>
         </QueryClientProvider>
     </React.StrictMode>,
 )
