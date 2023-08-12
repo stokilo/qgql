7787cc4d1d3905f8d54740f86b610392bd3b81de
Slawomir Stec <slawomirstec@gazeta.pl>
2023-08-12 08:40:35 +0400

    Update to graphql again.

--- vite/codegen.ts
+++ vite/codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
 
 const config: CodegenConfig = {
-    schema: 'curl http://localhost:8080/graphql/schema.graphql',
+    schema: 'http://localhost:8080/graphql/schema.graphql',
     documents: ['src/**/*.tsx'],
     generates: {
         './src/gql/': {
