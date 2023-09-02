a82f955959fa5b2f46b94fb71d72a942dd6e15dc
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-02 09:18:33 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/api/GQLResource.java
+++ src/main/java/com/sstec/qgql/api/GQLResource.java
@@ -1,6 +1,6 @@
 package com.sstec.qgql.api;
 
-import com.sstec.qgql.mapper.FavouritesMapper;
+import com.sstec.qgql.mapper.RootMapper;
 import com.sstec.qgql.model.gql.RootGQL;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.graphql.Description;
@@ -10,9 +10,9 @@ import org.eclipse.microprofile.graphql.Query;
 
 @GraphQLApi
 public class GQLResource {
-    @Inject
-    FavouritesMapper favouritesMapper;
 
+    @Inject
+    RootMapper rootMapper;
 
     @Query("getRoot")
     @Description("Get root state")
---
+++ src/main/java/com/sstec/qgql/mapper/JOOQGenerator.java
@@ -0,0 +1,44 @@
+package com.sstec.qgql.mapper;
+
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.jooq.codegen.GenerationTool;
+import org.jooq.meta.jaxb.*;
+
+@ApplicationScoped
+public class JOOQGenerator {
+
+    void onStart(@Observes StartupEvent ev) {
+        try {
+            Configuration configuration = new Configuration()
+                    .withJdbc(new Jdbc()
+                            .withDriver("org.postgresql.Driver")
+                            .withUrl("jdbc:postgresql://localhost:5432/quarkus")
+                            .withUser("quarkus")
+                            .withPassword("quarkus"))
+                    .withGenerator(new Generator()
+                            .withDatabase(new Database()
+                                    .withName("org.jooq.meta.postgres.PostgresDatabase")
+                                    .withIncludes(".*")
+                                    .withExcludes("")
+                                    .withIncludeIndexes(false)
+                                    .withIncludeSystemTables(false)
+                                    .withOutputCatalogToDefault(false)
+                                    .withInputSchema("public"))
+                            .withGenerate(new Generate()
+                                    .withPojos(true)
+                                    .withRelations(true)
+                            )
+                            .withTarget(new Target()
+                                    .withPackageName("com.sstec.qgql.model.generated")
+                                    .withDirectory("src/main/java")));
+
+            GenerationTool.generate(configuration);
+
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
