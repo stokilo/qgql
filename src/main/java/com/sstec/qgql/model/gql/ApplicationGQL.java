7def4a9aebcfb2a300941330d2de23f57c3632f6
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-02 09:10:34 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/model/gql/ApplicationGQL.java
+++ src/main/java/com/sstec/qgql/model/gql/ApplicationGQL.java
@@ -3,8 +3,9 @@ package com.sstec.qgql.model.gql;
 import com.sstec.qgql.model.generated.tables.pojos.Application;
 import com.sstec.qgql.model.generated.tables.pojos.Beneficiary;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApplicationGQL extends Application {
-    List<Beneficiary> beneficiaries;
+    public List<Beneficiary> beneficiaries = new ArrayList<>();
 }
