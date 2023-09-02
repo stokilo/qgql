795b4409a0e56dc7785d7c3bb633aa7d09e5ae35
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-02 19:04:04 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/mapper/RootMapper.java
+++ src/main/java/com/sstec/qgql/mapper/RootMapper.java
@@ -10,8 +10,6 @@ import io.smallrye.graphql.api.Context;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.jooq.DSLContext;
-import org.jooq.Record1;
-import org.jooq.Select;
 import org.jooq.SelectQuery;
 
 import java.util.List;
@@ -36,14 +34,6 @@ public class RootMapper {
         DataFetchingEnvironment dfe = context.unwrap(DataFetchingEnvironment.class);
         boolean withBeneficiaries = dfe.getSelectionSet().contains("RootGQL.applications/ApplicationGQL.beneficiaries");
 
-//        List<ApplicationGQL> applications = dsl.select(Application.APPLICATION,
-//                        multiset(select(Beneficiary.BENEFICIARY)
-//                                .from(Beneficiary.BENEFICIARY)
-//                                .where(Beneficiary.BENEFICIARY.APPLICATION_ID.eq(Application.APPLICATION.ID))
-//                        ).as("beneficiaries").convertFrom(r -> r.map(mapping(BeneficiaryGQL::new))))
-//                .from(Application.APPLICATION)
-//                .fetch().into(ApplicationGQL.class);
-
         SelectQuery<?> queryApp = dsl.select(Application.APPLICATION).getQuery();
         if (withBeneficiaries) {
             queryApp.addSelect(multiset(
@@ -55,71 +45,10 @@ public class RootMapper {
         queryApp.addFrom(Application.APPLICATION);
         List<ApplicationGQL> applications = queryApp.fetch().into(ApplicationGQL.class);
 
-
-        // This works with constructor that takes ActorRecord as parameter
-//        List<Film> result =
-//                dsl.select(
-//                                FILM.TITLE,
-//                                multiset(
-//                                        select(
-//                                                FILM_ACTOR.actor()
-//                                        )
-//                                                .from(FILM_ACTOR)
-//                                                .where(FILM_ACTOR.FILM_ID.eq(FILM.FILM_ID))
-//                                ).as("actors").convertFrom(r -> r.map(mapping(Actor::new)))
-//                        )
-//                        .from(FILM)
-//                        .orderBy(FILM.TITLE)
-//                        .fetch(mapping(Film::new));
-
-
         RootGQL rootGQL = new RootGQL();
         rootGQL.applications = applications;
         return rootGQL;
     }
 
-
-//    public  List<Favourites> getFavourites(Long userId) {
-//        DataFetchingEnvironment dfe = context.unwrap(DataFetchingEnvironment.class);
-//        boolean withDirector = dfe.getSelectionSet().contains("Favourites.movie/Movie.director");
-//
-//        CriteriaQuery<TodoList> cq = entityManager.getCriteriaBuilder().createQuery(TodoList.class);
-//        Root<TodoList> todoList = cq.from(TodoList.class);
-//        Fetch<TodoList, TodoItem> ownersFetch = todoList.fetch("items");
-//        // nested fetch
-//        List<TodoList> todolist = entityManager.createQuery(cq).getResultList();
-//
-//        //
-//        // can you add multiset conditionally to the query? -> looks like yes -> must be tested
-//
-////        SelectQuery<?> query = dsl.selectFrom(Director.DIRECTOR).getQuery();
-////        if (withDirector) {
-////            query.addSelect(multiset(
-////                    select(Movie.MOVIE.TITLE).from(Movie.MOVIE)
-////            ).convertFrom(r -> r.map(Records.mapping(Movie::new))));
-////        }
-////
-//        // is it possible to join multiple tables in this way (graphql) ?
-//        query.union()
-//
-////
-////        SelectSelectStep<Record2<String, Result<Record1<String>>>> s = dsl.select(
-////                Director.DIRECTOR.NAME,
-////                multiset(
-////                        select(Movie.MOVIE.TITLE).from(Movie.MOVIE)
-////                ).convertFrom(r -> r.map(Records.mapping(Movie::new)))
-////        ).from(Director.DIRECTOR);
-//
-//        TypedQuery<Favourites> query
-//                = entityManager.createQuery(
-//                "SELECT f FROM FAVOURITES f", Favourites.class);
-//        List<Favourites> resultList = query.getResultList();
-//        return resultList;
-//    }
-
-//    public Movie createMovie(Movie movie) {
-//
-//        return new Movie();
-//    }
 
 }
