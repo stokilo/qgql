df4c74a73a5e9a7ffa3cd88399e550e6ffc0cb4c
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-03 17:29:50 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/model/gql/ApplicationGQL.java
+++ src/main/java/com/sstec/qgql/model/gql/ApplicationGQL.java
@@ -1,6 +1,7 @@
 package com.sstec.qgql.model.gql;
 
 import com.sstec.qgql.model.generated.tables.pojos.Application;
+import org.jboss.logging.Logger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,14 +9,11 @@ import java.util.List;
 
 public class ApplicationGQL extends Application {
     public List<BeneficiaryGQL> beneficiaries = new ArrayList<>();
+    private static final Logger LOG = Logger.getLogger(ApplicationGQL.class);
 
     public ApplicationGQL(Application application, List<BeneficiaryGQL> beneficiaries) {
         super(application);
         this.beneficiaries = beneficiaries;
-    }
-
-    public ApplicationGQL(Application application){
-        super(application);
     }
 
     public ApplicationGQL(Integer id, String applicationNr, String contribution, String frequency) {
