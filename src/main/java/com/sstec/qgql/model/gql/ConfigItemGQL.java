e135465c2ed21a35b29421b46c7253e35373c5d1
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-03 17:29:59 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/model/gql/ConfigItemGQL.java
+++ src/main/java/com/sstec/qgql/model/gql/ConfigItemGQL.java
@@ -2,12 +2,18 @@ package com.sstec.qgql.model.gql;
 
 import com.sstec.qgql.model.generated.tables.pojos.ConfigItem;
 import com.sstec.qgql.model.generated.tables.records.ConfigItemRecord;
+import org.apache.commons.beanutils.PropertyUtilsBean;
+import org.jboss.logging.Logger;
 
 
 public class ConfigItemGQL extends ConfigItem {
+    private static final Logger LOG = Logger.getLogger(ConfigItemGQL.class);
 
     public ConfigItemGQL(ConfigItemRecord configItemRecord) {
-        super(configItemRecord.getId(), configItemRecord.getConfigId(), configItemRecord.getConfigKey(), configItemRecord.getConfigValue());
+        try {
+            new PropertyUtilsBean().copyProperties(this, configItemRecord);
+        } catch (Exception e) {
+            LOG.error("Exception while copying properties", e);
+        }
     }
-
 }
