13a34b7adcd4b39162066ae04ca1bc795b95c56f
Slawomir Stec <slawomirstec@gazeta.pl>
2023-09-04 20:32:05 +0400

    TODO1.

--- src/main/java/com/sstec/qgql/mapper/RootMapper.java
+++ src/main/java/com/sstec/qgql/mapper/RootMapper.java
@@ -1,74 +1,56 @@
 package com.sstec.qgql.mapper;
 
-import com.sstec.qgql.model.generated.tables.Application;
-import com.sstec.qgql.model.generated.tables.Beneficiary;
-import com.sstec.qgql.model.generated.tables.Config;
-import com.sstec.qgql.model.generated.tables.ConfigItem;
-import com.sstec.qgql.model.gql.ApplicationGQL;
-import com.sstec.qgql.model.gql.BeneficiaryGQL;
-import com.sstec.qgql.model.gql.ConfigGQL;
-import com.sstec.qgql.model.gql.ConfigItemGQL;
 import com.sstec.qgql.model.gql.RootGQL;
-import graphql.schema.DataFetchingEnvironment;
 import io.smallrye.graphql.api.Context;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import org.jooq.DSLContext;
-import org.jooq.SelectQuery;
-
-import java.util.List;
-
-import static org.jooq.Records.mapping;
-import static org.jooq.impl.DSL.multiset;
-import static org.jooq.impl.DSL.select;
 
 
 @ApplicationScoped
 public class RootMapper {
 
-
     @Inject
     Context context;
 
-    @Inject
-    DSLContext dsl;
-
-
     public RootGQL getRoot(Long applicationId) {
-        RootGQL rootGQL = new RootGQL();
-
-        DataFetchingEnvironment dfe = context.unwrap(DataFetchingEnvironment.class);
-        boolean withBeneficiaries = dfe.getSelectionSet().contains("RootGQL.applications/ApplicationGQL.beneficiaries");
-        boolean withConfig = dfe.getSelectionSet().contains("RootGQL.config");
-
-        SelectQuery<?> queryApp = dsl.select(Application.APPLICATION).getQuery();
-        if (withBeneficiaries) {
-            queryApp.addSelect(multiset(
-                    select(Beneficiary.BENEFICIARY)
-                            .from(Beneficiary.BENEFICIARY)
-                            .where(Beneficiary.BENEFICIARY.APPLICATION_ID.eq(Application.APPLICATION.ID))
-            ).as("beneficiaries").convertFrom(r -> r.map(mapping(BeneficiaryGQL::new))));
-        }
-        queryApp.addFrom(Application.APPLICATION);
-        List<ApplicationGQL> applications = queryApp.fetch().into(ApplicationGQL.class);
-
-        if (withConfig) {
-            SelectQuery<?> queryConfig = dsl.select(Config.CONFIG).getQuery();
-            boolean withConfigItems = dfe.getSelectionSet().contains("RootGQL.config/ConfigGQL.items");
-            if (withConfigItems) {
-                queryConfig.addSelect(multiset(
-                        select(ConfigItem.CONFIG_ITEM)
-                                .from(ConfigItem.CONFIG_ITEM)
-                                .where(ConfigItem.CONFIG_ITEM.CONFIG_ID.eq(Config.CONFIG.ID))
-                ).as("items").convertFrom(r -> r.map(mapping(ConfigItemGQL::new))));
-            }
-            queryConfig.addFrom(Config.CONFIG);
-            rootGQL.config = queryConfig.fetchOne().into(ConfigGQL.class);
-        }
-
-
-        rootGQL.applications = applications;
-        return rootGQL;
+        return new RootGQL();
     }
+
+//    public RootGQL getRoot(Long applicationId) {
+//        RootGQL rootGQL = new RootGQL();
+//
+//        DataFetchingEnvironment dfe = context.unwrap(DataFetchingEnvironment.class);
+//        boolean withBeneficiaries = dfe.getSelectionSet().contains("RootGQL.applications/ApplicationGQL.beneficiaries");
+//        boolean withConfig = dfe.getSelectionSet().contains("RootGQL.config");
+//
+//        SelectQuery<?> queryApp = dsl.select(Application.APPLICATION).getQuery();
+//        if (withBeneficiaries) {
+//            queryApp.addSelect(multiset(
+//                    select(Beneficiary.BENEFICIARY)
+//                            .from(Beneficiary.BENEFICIARY)
+//                            .where(Beneficiary.BENEFICIARY.APPLICATION_ID.eq(Application.APPLICATION.ID))
+//            ).as("beneficiaries").convertFrom(r -> r.map(mapping(BeneficiaryGQL::new))));
+//        }
+//        queryApp.addFrom(Application.APPLICATION);
+//        List<ApplicationGQL> applications = queryApp.fetch().into(ApplicationGQL.class);
+//
+//        if (withConfig) {
+//            SelectQuery<?> queryConfig = dsl.select(Config.CONFIG).getQuery();
+//            boolean withConfigItems = dfe.getSelectionSet().contains("RootGQL.config/ConfigGQL.items");
+//            if (withConfigItems) {
+//                queryConfig.addSelect(multiset(
+//                        select(ConfigItem.CONFIG_ITEM)
+//                                .from(ConfigItem.CONFIG_ITEM)
+//                                .where(ConfigItem.CONFIG_ITEM.CONFIG_ID.eq(Config.CONFIG.ID))
+//                ).as("items").convertFrom(r -> r.map(mapping(ConfigItemGQL::new))));
+//            }
+//            queryConfig.addFrom(Config.CONFIG);
+//            rootGQL.config = queryConfig.fetchOne().into(ConfigGQL.class);
+//        }
+//
+//
+//        rootGQL.applications = applications;
+//        return rootGQL;
+//    }
 
 }
--- src/main/java/com/sstec/qgql/model/gql/RootGQL.java
+++ src/main/java/com/sstec/qgql/model/gql/RootGQL.java
@@ -1,12 +1,9 @@
 package com.sstec.qgql.model.gql;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class RootGQL {
 
     public String test;
-    public List<ApplicationGQL> applications = new ArrayList<>();
-    public ConfigGQL config;
+//    public List<ApplicationGQL> applications = new ArrayList<>();
+//    public ConfigGQL config;
 }
