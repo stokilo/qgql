5a5d980f47b90e6df04f5b1b5aa196c9163f9027
Slawomir Stec <slawomirstec@gazeta.pl>
2024-06-13 19:09:01 +0400

    Login page.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client'
 import {Provider, createClient, cacheExchange, fetchExchange} from "urql";
 import Lead from "./lead/Lead";
 import LeadDashboard from "./App";
+import LoginPage from "./auth/LoginPage";
+import CssBaseline from "@mui/joy/CssBaseline";
+import GlobalStyles from "@mui/joy/GlobalStyles";
 
 const client = createClient({
     url: 'http://localhost:8080/graphql',
@@ -11,10 +14,20 @@ const client = createClient({
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-            <Provider value={client}>
+        <Provider value={client}>
+            <CssBaseline />
+            <GlobalStyles
+                styles={{
+                    ':root': {
+                        '--Form-maxWidth': '800px',
+                        '--Transition-duration': '0.4s', // set to `none` to disable transition
+                    },
+                }}
+            />
             {/*<ReactQueryDevtools initialIsOpen={false} />*/}
             {/*        <Lead/>*/}
-            <LeadDashboard/>
-            </Provider>
-     </React.StrictMode>
+            {/*<LeadDashboard/>*/}
+            <LoginPage/>
+        </Provider>
+    </React.StrictMode>
 )
