c89897d631144318cd7daf8d62c563b0b6e38045
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-04 15:49:33 +0400

    Update.

--- src/main/java/com/sstec/qgql/api/HouseResource.java
+++ src/main/java/com/sstec/qgql/api/HouseResource.java
@@ -2,12 +2,14 @@ package com.sstec.qgql.api;
 
 import com.sstec.qgql.mapper.HouseMapper;
 import com.sstec.qgql.model.entity.house.House;
+import io.quarkus.security.Authenticated;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.graphql.*;
 
 import java.util.List;
 
 @GraphQLApi
+@Authenticated
 public class HouseResource {
 
     @Inject
