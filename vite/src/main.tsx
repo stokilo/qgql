23e70ef8f39dc14b66aac5fd2df7f8c2f900a8f4
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-09 09:47:41 +0400

    Radix test.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -1,46 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Provider, createClient, cacheExchange, fetchExchange} from "urql";
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
-import LoginPage from "./auth/LoginPage";
-import CssBaseline from "@mui/joy/CssBaseline";
-import GlobalStyles from "@mui/joy/GlobalStyles";
-import LeadDashboardPage from "./lead/LeadDashboardPage";
-import App from "./auth/LoginPage";
+import {Badge} from "./components/ui/Badge";
 
 const client = createClient({
     url: 'http://localhost:8080/graphql',
     exchanges: [cacheExchange, fetchExchange]
 })
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App/>,
-    },
-    {
-        path: "/lead-dashboard",
-        element:    <LeadDashboardPage/>,
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
         <Provider value={client}>
-            <RouterProvider router={router} />
-            <CssBaseline />
-            <GlobalStyles
-                styles={{
-                    ':root': {
-                        '--Form-maxWidth': '800px',
-                        '--Transition-duration': '0.4s', // set to `none` to disable transition
-                    },
-                }}
-            />
-            {/*<ReactQueryDevtools initialIsOpen={false} />*/}
+            <Badge/>
         </Provider>
     </React.StrictMode>
 )
