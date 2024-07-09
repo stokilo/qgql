2a3b1262a5f9479d831cad9a3a43cd081eeb2a44
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-09 10:24:51 +0400

    Radix test.

--- vite/src/main.tsx
+++ vite/src/main.tsx
@@ -16,15 +16,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
             <Badge type="info" size="sm">
                 <span>Info message</span>
             </Badge>
-
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-
-            <span className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">
-            Hello<br/>
-            World
-            </span>
         </Provider>
     </React.StrictMode>
 )
