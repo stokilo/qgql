2c7c8d8cbca7873812b4b2391b1221c38476c8a5
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-10 16:31:41 +0400

    Radix test.

--- src/main/java/com/sstec/qgql/App.java
+++ src/main/java/com/sstec/qgql/App.java
@@ -17,29 +17,27 @@ public class App {
     private static final Logger log = Logger.getLogger(App.class);
 
     void onStart(@Observes StartupEvent ev) {
-        if (ProfileManager.getLaunchMode().isDevOrTest()) {
-            new java.util.Timer().schedule(
-                    new java.util.TimerTask() {
-                        @Override
-                        public void run() {
-                            try {
-                                log.info("Generate schema under web/generated dir");
-                                ProcResult schema = new ProcBuilder("curl", "" +
-                                        "http://localhost:8080/graphql/schema.graphql").run();
-                                Files.write(Paths.get("web/gql/schema.gql"), schema.getOutputBytes());
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        try {
+                            log.info("Generate schema under web/generated dir");
+                            ProcResult schema = new ProcBuilder("curl", "" +
+                                    "http://localhost:8080/graphql/schema.graphql").run();
+                            Files.write(Paths.get("web/gql/schema.gql"), schema.getOutputBytes());
 
-                                log.info("Start conversion from gql to ts");
-                                ProcResult output = new ProcBuilder("yarn", "codegen")
-                                        .withWorkingDirectory(new File("web")).run();
-                                log.info(output.getOutputString());
-                            } catch (Exception e) {
-                                log.error(e.getMessage(), e);
-                            }
+                            log.info("Start conversion from gql to ts");
+                            ProcResult output = new ProcBuilder("yarn", "codegen")
+                                    .withWorkingDirectory(new File("web")).run();
+                            log.info(output.getOutputString());
+                        } catch (Exception e) {
+                            log.error(e.getMessage(), e);
                         }
-                    },
-                    1000
-            );
-        }
+                    }
+                },
+                1000
+        );
     }
 
 }
