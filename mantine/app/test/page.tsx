c4acb2b0c44cfc88c31b4502e9ce8b730ed808de
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-10 15:22:04 +0400

    Radix test.

---
+++ mantine/app/test/page.tsx
@@ -0,0 +1,11 @@
+import { DoubleNavbar } from '@/components/DoubleNavbar/DoubleNavbar';
+
+export default function HomePage2() {
+  return (
+    <>
+      <DoubleNavbar></DoubleNavbar>
+      {/* <Welcome /> */}
+      {/* <ColorSchemeToggle /> */}
+    </>
+  );
+}
