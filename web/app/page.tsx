6594060023f5833a131aa88130ec62222ac9416c
Slawomir Stec <slawomir.stec@forwardyou.com>
2024-07-12 08:32:56 +0400

    Keycloack.

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   return (
     <>
       Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      <button onClick={() => signIn('keycloak')}>Sign in</button>
     </>
   );
 }
